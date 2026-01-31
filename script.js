e73da8788c254f37f4ba78267ca3a8c136112dcc
Tushar Raghuvanshi <79913268+T7351@users.noreply.github.com>
2026-01-31 10:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-//your JS code here. If required.
+    const circles = document.querySelectorAll('.circle');
+    const progress = document.getElementById('progress');
+    const prev = document.getElementById('prev');
+    const next = document.getElementById('next');
+
+    let current = 1;
+    next.addEventListener('click',() => {
+        current++;
+        if(current > circles.length) {
+            current = circles.length;
+        }
+        update();
+    });
+    prev.addEventListener('click',() => {
+        current--;
+        if(current < 1){
+            current = 1;
+        }
+        update();
+    })
+    function update(){
+        circles.forEach((circle, index) =>{
+            if(index < current){
+                circle.classList.add('active');
+            }else{
+                circle.classList.remove('active');
+            }
+        })
+        const activeCircles = document.querySelectorAll(".circle.active");
+        progress.style.width = ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%";
+        prev.disabled = current === 1;
+        next.disabled = current === circles.length;
+    }
